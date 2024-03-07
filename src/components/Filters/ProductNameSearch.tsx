99fe9a1e467ba1c42a8bee63d22fc5fd75d7de09
souldrum <soul_20585@mail.ru>
2024-03-07 09:55:03 +0500

    ProductNameSearch: add focus-within to div className

--- src/components/Filters/ProductNameSearch.tsx
+++ src/components/Filters/ProductNameSearch.tsx
@@ -1,33 +1,21 @@
-import cn from "classnames";
 import React from "react";
 
 export const ProductNameSearch: React.FC<{
   onChange: (value: string) => void;
 }> = ({ onChange }) => {
   const [value, setValue] = React.useState("");
-  const [focus, setFocus] = React.useState(false);
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) =>
     setValue(e.target.value);
 
-  const handleFocus = () => setFocus(true);
-
-  const handleBlur = () => {
-    onChange(value);
-    setFocus(false);
-  };
+  const handleBlur = () => onChange(value);
 
   const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
     if (e.key === "Enter") onChange(value);
   };
 
   return (
-    <div
-      className={cn(
-        "border-b border-outline-variant pb-3 pl-4 min-w-input text-outline text-body-lg bg-inherit",
-        focus && "border-b-2"
-      )}
-    >
+    <div className="border-b focus-within:border-b-2 border-outline-variant pb-3 pl-4 min-w-input text-outline text-body-lg bg-inherit">
       <input
         className="pt-2 bg-transparent outline-none autofill:shadow-input-autocomplete w-11/12 placeholder:text-outline-variant"
         type="text"
@@ -38,7 +26,6 @@ export const ProductNameSearch: React.FC<{
         onChange={handleChange}
         onBlur={handleBlur}
         onKeyDown={handleKeyDown}
-        onFocus={handleFocus}
       />
     </div>
   );
